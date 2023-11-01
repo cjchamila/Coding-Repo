45e63c794eae9c3638eca583bfe4b0357490acde
Chamila Jayaratne <cjchamila@gmail.com>
2023-11-01 13:19:24 +0530

    add fields and constructors

--- src/com/chamila/coding/trees/Node.java
+++ src/com/chamila/coding/trees/Node.java
@@ -1,5 +1,18 @@
 package com.chamila.coding.trees;
 
-public class Node {
+public class Node<T> {
+	private Node<T>left;
+	private Node<T>right;
+	private int data;
+	
+	public Node(int data){		
+		this(null,null,data);
+	}
+	
+	public Node(Node left,Node right,int data) {
+		this.left=left;
+		this.right=right;
+		this.data=data;
+	}
 
 }
